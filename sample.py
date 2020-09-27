129e0934822a63ba414aa149c08fffb914b3a96c
佐橘一旗 <1tsuki@s-230.local>
2020-09-28 06:50:45 +0900

    solved items up to ex03

---
+++ sample.py
@@ -0,0 +1,20 @@
+import pyxel
+
+
+class App:
+    def __init__(self):
+        pyxel.init(200, 150, caption="Pyxel Sample App")
+        pyxel.mouse(True)
+        pyxel.run(self.update, self.draw)
+
+    def update(self):
+        if pyxel.btnp(pyxel.KEY_Q):
+            pyxel.quit()
+
+    def draw(self):
+        pyxel.cls(1)
+
+        pyxel.text(16, 17, "q: Quit App", 14)
+
+
+App()
